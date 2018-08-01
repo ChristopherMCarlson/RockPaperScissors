31241f5e41b859b18f9ec1e783d73c6680620ebf
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-01 13:45:21 -0600

    game works

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 function play(playerChoice){
-    let compChoice = "rock";
-    if(playerChoice = "paper"){
-        
+    let compChoice = Math.floor((Math.random() * 3) + 1);
+    if (playerChoice == 1){
+        if (playerChoice == compChoice){
+            document.getElementById("message").innerHTML = '<p>Draw!</p>';
+        } else if (playerChoice + 1 == compChoice){
+            document.getElementById("message").innerHTML = '<p>You lose!</p>';
+        } else if (playerChoice + 2 == compChoice){
+            document.getElementById("message").innerHTML = '<p>You win!</p>';
+        }
+    } else if (playerChoice == 2){
+        if (playerChoice > compChoice){
+            document.getElementById("message").innerHTML = '<p>You win!</p>';
+        } else if (playerChoice == compChoice){
+            document.getElementById("message").innerHTML = '<p>Draw!</p>';
+        } else if (playerChoice < compChoice){
+            document.getElementById("message").innerHTML = '<p>You lose!</p>';
+        }
+    } else if (playerChoice == 3){
+        if (playerChoice - 2 == compChoice){
+            document.getElementById("message").innerHTML = '<p>You lose!</p>';
+        } else if (playerChoice - 1 == compChoice){
+            document.getElementById("message").innerHTML = '<p>You win!</p>';
+        } else if (playerChoice == compChoice){
+            document.getElementById("message").innerHTML = '<p>Draw!</p>';
+        }
     }
 }
